99432b6be7e16cb2cd480ab9afa0b025f84688f7
Janakiram-2005 <msjanakiram2005@gmail.com>
2026-08-23 15:41:57 +0530

    Update Express catch-all route to use regex for compatibility

--- server.js
+++ server.js
@@ -445,7 +445,7 @@ io.on('connection', (socket) => {
 // Serve frontend static files
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
