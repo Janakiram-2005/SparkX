702a52222716f577dbf872f08ad41e10c78c518d
Janakiram-2005 <msjanakiram2005@gmail.com>
2026-08-28 00:56:06 +0530

    Chore: Clean up unnecessary files and images

--- prepare_tailwind.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('generated_dashboard.html', 'utf8');
-const scriptMatch = html.match(/<script id="tailwind-config">([\s\S]*?)<\/script>/i);
-if (scriptMatch) {
-  let configStr = scriptMatch[1].replace('tailwind.config =', 'export default');
-  configStr = configStr.replace(/theme:/, 'content: [\"scratch_jsx.txt\"],\n  corePlugins: { preflight: false },\n  theme:');
-  fs.writeFileSync('tailwind.admin.config.js', configStr);
-  console.log('Saved to tailwind.admin.config.js');
-}
